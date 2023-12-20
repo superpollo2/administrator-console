91598abc5a785c3793170a5dd91ec53d50646606
David Upegui <dupegui2002@gmail.com>
2023-12-20 08:56:26 -0500

    Footer component styles are now managed with css modules
    Footer component has now his own Footer.module.css file and its styles
    are managed from that file using css modules.

--- components/Layout/footer/Footer.tsx
+++ components/Layout/footer/Footer.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
+import styles from '@/styles/Footer.module.css'
 
 const Footer = () => {
 	return (
-		<footer className='container-footer'>
-			<div className='container-footer--text'>
-				<div className='text-center'>
+		<footer className={styles.footerContainer}>
+			<div className={styles.textFooterContainer}>
+				<div className={styles.footerText}>
 					<img src='images/logoudea.svg' alt='logoudea' width={250}/>
 					
 				</div>
-				<div className='text-center'>
+				<div className={styles.footerText}>
 					Laboratorio Integrado de Sistemas <br/>
                     Departamento de Ingeniería de
 					Sistemas<br/>
                     Universidad de Antioquia
 				</div>
-				<div className='text-center'>
+				<div className={styles.footerText}>
 					Medellín - Colombia <br/>
                     Dirección: calle 67 No. 53 - 108<br/>
                     Teléfono: [57+604] 219 5532<br/>
                     Oficina: 18-210
 				</div>
-				<div className='text-center'>
+				<div className={styles.footerText}>
 					Recepción de correspondencia:<br/>
                     calle 70 No. 52 - 21<br/>
                     Apartado Aéreo 1226
 				</div>
-                <div className='text-center'>
+                <div className={styles.footerText}>
                 <span>
 						©2023 Laboratorio Integrado de Sistemas
                         <br/>Departamento de
