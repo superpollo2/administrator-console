d6ce5c054579cb3c8fde97cadea99f31e8659129
David Upegui <dupegui2002@gmail.com>
2023-12-22 14:36:00 -0500

    Navigation links  are now managed with Link component from NextJS
    The Link is inside the NavLink component. The route changes are now
    managed using Link component instead of HTML button and push route
    functions.

--- components/Layout/SideNav/NavLinks.tsx
+++ components/Layout/SideNav/NavLinks.tsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { useRouter, usePathname } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { getLinks } from '@/services/getLinks';
 import NavLink from './NavLink';
 import styles from '@/styles/SideNav.module.css'
 
 const NavLinks = () => {
 	const links = getLinks();
-	const router = useRouter();
 	const pathName = usePathname();
 	const [activeLink, setActiveLink] = useState('');
 
@@ -19,18 +18,8 @@ const NavLinks = () => {
 	return (
 		<ul className={styles.ul}>
 			{links.map(({ label, route, icon: Icon }) => (
-				<li key={route} className={`${styles.li} ${route === activeLink ? `${styles.active}` : ''}`}>
-					<button
-						onClick={() => {
-							router.push(route);
-						}}
-						className={styles.link}
-						type='button'
-					>
-						{Boolean(Icon) && <Icon />}
-						<span>{label}</span>
-					</button>
-					{/* <NavLink label={label} route={route} icon={Icon} /> */}
+				<li key={route} className={`${styles.li} ${route === activeLink && `${styles.active}`}`}>
+					<NavLink label={label} route={route} icon={Icon} />
 				</li>
 			))}
 		</ul>
