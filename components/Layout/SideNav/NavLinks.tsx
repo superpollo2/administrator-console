bf41fb2cd4b6f1aa5f990e246b4ac200d3945220
David Upegui <dupegui2002@gmail.com>
2023-12-22 14:55:23 -0500

    Sonar Fix: No more nested template literals

--- components/Layout/SideNav/NavLinks.tsx
+++ components/Layout/SideNav/NavLinks.tsx
@@ -8,17 +8,18 @@ const NavLinks = () => {
 	const links = getLinks();
 	const pathName = usePathname();
 	const [activeLink, setActiveLink] = useState('');
+	const active = false
 
 	useEffect(() => {
 		// Actualiza el estado de activeLink cuando cambia la ruta
-		setActiveLink(pathName);
+		setActiveLink(pathName); 
 	}, [pathName]);
 
 
 	return (
 		<ul className={styles.ul}>
 			{links.map(({ label, route, icon: Icon }) => (
-				<li key={route} className={`${styles.li} ${route === activeLink && `${styles.active}`}`}>
+				<li key={route} className={`${styles.li} ${route === activeLink && styles.active}`}>
 					<NavLink label={label} route={route} icon={Icon} />
 				</li>
 			))}
