dacaef948d13de825cfe67f13d0f171e6623f2e3
David Upegui <dupegui2002@gmail.com>
2024-01-11 16:43:30 -0500

    Improved: body html structure
    The body tag has now all the css instead of a div inside the body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/react-in-jsx-scope */
 'use client';
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 import { Inter } from 'next/font/google';
 import { SideNav } from '@/components/Layout/SideNav/SideNav';
 import { TopBar } from '@/components/Layout/topBar/TopBar';
 import { Footer } from '@/components/Layout/footer/Footer';
-import sideNavstyles from '@/styles/SideNav.module.css'
-import layoutStyles from '@/styles/Layout.module.css'
+import sideNavstyles from '@/styles/SideNav.module.css';
+import layoutStyles from '@/styles/Layout.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,15 +20,13 @@ const RootLayout = ({ children }: RootLayoutProps) => {
 			<head>
 				<title>Consola de Administrador</title>
 			</head>
-			<body className={`${inter.className} ${layoutStyles.body}`}>
-				<div className={layoutStyles.bodyContainer}>
-						<SideNav />
-					<div className={layoutStyles.bodyContent}>
-						<div className={layoutStyles.topBarContainer}>
-							<TopBar />
-						</div>
-						{children}
+			<body className={`${inter.className} ${layoutStyles.bodyContainer}`}>
+				<SideNav />
+				<div className={layoutStyles.bodyContent}>
+					<div className={layoutStyles.topBarContainer}>
+						<TopBar />
 					</div>
+					{children}
 				</div>
 				<Footer />
 			</body>
